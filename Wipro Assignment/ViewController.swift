3e112621a59281ded7b4ff021e358764ebfa018e
i-am-sambit <sambitprakash28@gmil.com>
2020-07-30 18:39:35 +0530

    Initial Commit

---
+++ Wipro Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wipro Assignment
+//
+//  Created by SAMBIT DASH on 30/07/20.
+//  Copyright © 2020 SAMBIT DASH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
